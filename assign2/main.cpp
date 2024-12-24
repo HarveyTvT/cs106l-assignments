18ad45b966be72b515c62ebd9ffcbb19638cde96
shihaoyu <shihaoyu@echo.tech>
2024-12-25 03:51:47 +0800

    update class

--- assign2/main.cpp
+++ assign2/main.cpp
@@ -1,6 +1,7 @@
 /*
  * CS106L Assignment 2: Marriage Pact
- * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
+ * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob
+ * Roberts-Baca.
  *
  * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
  * in this file. You do not need to modify any other files.
@@ -16,47 +17,91 @@
 
 #include "utils.h"
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "Connor Anderson"; // Don't forget to change this!
 
 /**
- * Takes in a file name and returns a set containing all of the applicant names as a set.
+ * Takes in a file name and returns a set containing all of the applicant names
+ * as a set.
  *
  * @param filename  The name of the file to read.
  *                  Each line of the file will be a single applicant's name.
  * @returns         A set of all applicant names read from the file.
  *
- * @remark Feel free to change the return type of this function (and the function
- * below it) to use a `std::unordered_set` instead. If you do so, make sure
- * to also change the corresponding functions in `utils.h`.
+ * @remark Feel free to change the return type of this function (and the
+ * function below it) to use a `std::unordered_set` instead. If you do so, make
+ * sure to also change the corresponding functions in `utils.h`.
  */
-std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
+std::unordered_set<std::string> get_applicants(std::string filename) {
+    // STUDENT TODO: Implement this function.
+    std::ifstream ifs("students.txt");
+    std::unordered_set<std::string> results;
+
+    if (ifs.is_open()) {
+        std::string line;
+        while (std::getline(ifs, line)) {
+            results.insert(line);
+        }
+    }
+
+    return results;
 }
 
 /**
  * Takes in a set of student names by reference and returns a queue of names
  * that match the given student name.
  *
- * @param name      The returned queue of names should have the same initials as this name.
+ * @param name      The returned queue of names should have the same initials as
+ * this name.
  * @param students  The set of student names.
  * @return          A queue containing pointers to each matching name.
  */
-std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
-  // STUDENT TODO: Implement this function.
+std::queue<const std::string*>
+find_matches(std::string name, std::unordered_set<std::string>& students) {
+    std::queue<const std::string*> results{};
+
+    char init_f = name.at(0);
+    char init_l = name.at(name.find(" ") + 1);
+
+    for (const std::string& student : students) {
+        char f = student.at(0);
+        char l = student.at(student.find(" ") + 1);
+
+        if (init_f == f && init_l == l) {
+            results.push(&student);
+        }
+    }
+
+    std::cout << "find_matches size" << results.size() << "\n";
+
+    return results;
 }
 
 /**
- * Takes in a queue of pointers to possible matches and determines the one true match!
+ * Takes in a queue of pointers to possible matches and determines the one true
+ * match!
  *
- * You can implement this function however you'd like, but try to do something a bit
- * more complicated than a simple `pop()`.
+ * You can implement this function however you'd like, but try to do something a
+ * bit more complicated than a simple `pop()`.
  *
  * @param matches The queue of possible matches.
  * @return        Your magical one true love.
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
 std::string get_match(std::queue<const std::string*>& matches) {
-  // STUDENT TODO: Implement this function.
+    if (matches.empty()) {
+        return "NO MATCHES FOUND";
+    }
+
+    std::string match;
+    while (!matches.empty()) {
+        const std::string& applicant = *matches.front();
+        matches.pop();
+        if (applicant.length() > match.length()) {
+            match = applicant;
+        }
+    }
+
+    return match;
 }
 
 /* #### Please don't modify this call to the autograder! #### */
