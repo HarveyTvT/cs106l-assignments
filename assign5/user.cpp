1ad0f2e5858d5582345327b0eabd5c645131bb55
shihaoyu <shihaoyu@echo.tech>
2024-12-30 05:18:43 +0800

    assign 5

--- assign5/user.cpp
+++ assign5/user.cpp
@@ -4,62 +4,93 @@
  * Creates a new User with the given name and no friends.
  */
 User::User(const std::string& name)
-  : _name(name)
-  , _friends(nullptr)
-  , _size(0)
-  , _capacity(0)
-{
-}
+    : _name(name), _friends(nullptr), _size(0), _capacity(0) {}
 
 /**
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
  */
-void
-User::add_friend(const std::string& name)
-{
-  if (_size == _capacity) {
-    _capacity = 2 * _capacity + 1;
-    std::string* newFriends = new std::string[_capacity];
-    for (size_t i = 0; i < _size; ++i) {
-      newFriends[i] = _friends[i];
+void User::add_friend(const std::string& name) {
+    if (_size == _capacity) {
+        _capacity = 2 * _capacity + 1;
+        std::string* newFriends = new std::string[_capacity];
+        for (size_t i = 0; i < _size; ++i) {
+            newFriends[i] = _friends[i];
+        }
+        delete[] _friends;
+        _friends = newFriends;
     }
-    delete[] _friends;
-    _friends = newFriends;
-  }
 
-  _friends[_size++] = name;
+    _friends[_size++] = name;
 }
 
 /**
  * Returns the name of this User.
  */
-std::string
-User::get_name() const
-{
-  return _name;
-}
+std::string User::get_name() const { return _name; }
 
 /**
  * Returns the number of friends this User has.
  */
-size_t
-User::size() const
-{
-  return _size;
-}
+size_t User::size() const { return _size; }
 
 /**
  * Sets the friend at the given index to the given name.
  * @param index The index of the friend to set.
  * @param name The name to set the friend to.
  */
-void User::set_friend(size_t index, const std::string& name)
-{
-  _friends[index] = name;
+void User::set_friend(size_t index, const std::string& name) {
+    _friends[index] = name;
 }
 
-/** 
+/**
  * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
+ * The definitions for your custom operators and special member functions will
+ * go here!
  */
+std::ostream& operator<<(std::ostream& s, const User& user) {
+    s << "User(name=" << user._name << ", friends=[";
+    for (int i = 0; i < user._size; i++) {
+        s << user._friends[i];
+        if (i < user._size - 1) {
+            s << ", ";
+        }
+    }
+    s << "])";
+    return s;
+}
+
+User::~User() { delete[] _friends; }
+
+User::User(const User& user) {
+    _name = user.get_name();
+    _friends = new std::string[user.size()];
+    _size = user._size;
+    _capacity = user._capacity;
+
+    for (int i = 0; i < user.size(); i++) {
+        _friends[i] = user._friends[i];
+    }
+}
+
+User& User::operator=(const User& user) {
+    _name = user._name;
+    _size = user._size;
+    _capacity = user._capacity;
+    delete[] _friends;
+    _friends = new std::string[user._capacity];
+    for (int i = 0; i < user._size; i++) {
+        _friends[i] = user._friends[i];
+    }
+    return *this;
+}
+
+User& User::operator+=(User& other) {
+    add_friend(other.get_name());
+    other.add_friend(_name);
+    return *this;
+}
+
+bool User::operator<(const User& other) const {
+    return _name < other.get_name();
+}
--- assign5/user.h
+++ assign5/user.h
@@ -6,23 +6,31 @@
 #include <iostream>
 #include <string>
 
-class User
-{
-public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
-  std::string get_name() const;
-  size_t size() const;
-  void set_friend(size_t index, const std::string& name);
+class User {
+  public:
+    User(const std::string& name);
+    void add_friend(const std::string& name);
+    std::string get_name() const;
+    size_t size() const;
+    void set_friend(size_t index, const std::string& name);
 
-  /** 
-   * STUDENT TODO:
-   * Your custom operators and special member functions will go here!
-   */
+    /**
+     * STUDENT TODO:
+     * Your custom operators and special member functions will go here!
+     */
+    friend std::ostream& operator<<(std::ostream& s, const User& user);
 
-private:
-  std::string _name;
-  std::string* _friends;
-  size_t _size;
-  size_t _capacity;
+    ~User();
+    User(const User& user);
+    User& operator=(const User& user);
+    User(const User&& user) = delete;
+    User& operator=(const User&& user) = delete;
+    User& operator+=(User& other);
+    bool operator<(const User& other) const;
+
+  private:
+    std::string _name;
+    std::string* _friends;
+    size_t _size;
+    size_t _capacity;
 };
